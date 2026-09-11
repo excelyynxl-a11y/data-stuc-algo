df113e83ce738682365ba24d7bc7c597b3598384
excelyynxl-a11y <excelyynxl@gmail.com>
2026-09-11 12:17:46 +0800

    reinvent graph with algos (todo)

---
+++ graph_pro_version.py
@@ -0,0 +1,53 @@
+class Graph():
+    def __init__(self, is_adjacency_list=True):
+         pass 
+
+    def __len__(self):
+         pass 
+
+    def bfs(self):
+         pass 
+
+    def dfs(self): 
+         pass 
+
+    def dijkstra(self):
+         pass 
+
+    def kahns(self):
+         pass 
+
+    def topological_sort_dfs(self):
+         pass 
+
+    def prims(self):
+         pass 
+
+    def kruskal(self):
+         pass 
+
+    def bellman_ford(self):
+         pass 
+
+    def floyd_warshall(self):
+         pass
+
+    def reset(self):
+         pass 
+
+    def __str__(self):
+          pass 
+
+class Vertex():
+     def __init__(self):
+          pass 
+
+     def __str__(self):
+          pass 
+
+class Edge():
+     def __init__(self, is_directed=False, is_weighted=False):
+          pass 
+
+     def __str__(self):
+          pass 
