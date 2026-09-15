f41f3b813a782a0200b4de293cc7d15682763931
excelyynxl-a11y <excelyynxl@gmail.com>
2026-09-15 13:51:53 +0800

    insert

--- trie.py
+++ trie.py
@@ -2,7 +2,40 @@ class Trie:
     def __init__(self):
         self.root = Node()
 
+    def insert(self, key, data=None):
+        # start from root  
+        current = self.root
+
+        # iterate each character in key
+        for char in key:
+            index = ord(char) - 97 + 1
+            print(index, char)
+
+            # if path exist, move to next node
+            if not current.links[index] is None:
+                current = current.links[index]
+            # if path doesnt exist, create a new node and move to the new node
+            else:
+                current.links[index] = Node()
+                current = current.links[index]
+
+        # go through terminal character ($)
+        index = 0 
+        if not current.links[index] is None:
+            current = current.links[index]
+        else:
+            current.links[index] = Node()
+            current = current.links[index]
+
+        # add data 
+        current.data = data 
+
+
 class Node: 
-    def __init__(self, data=None):
+    def __init__(self, data=None, size=27):
         self.data = data 
-        self.links = [None] * 27
+        self.links = [None] * size
+
+
+trie_test = Trie()
+trie_test.insert("oklo", "sap baby")
